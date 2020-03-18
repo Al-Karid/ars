3dfbeafbcd7b84626deaa2f2e1d0be7150418315
Al-Karid <grencisse@hotmail.com>
2020-03-18 11:50:23 +0000

    Fix flask dep

--- setup.py
+++ setup.py
@@ -12,7 +12,8 @@ setuptools.setup(
         "click==7.0",
         "pandas==1.0.1",
         "beautifulsoup4==4.8.2",
-        "requests==2.21.0"
+        "requests==2.21.0",
+        "flask==1.1.1"
         ],
     entry_points='''
     [console_scripts]
